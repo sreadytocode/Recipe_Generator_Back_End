7ba8a53226c5f850baa8a0a74a0f41baa02b022b
sreadytocode <sara.younis2303@gmail.com>
2022-12-16 15:12:35 +0000

    First commit

---
+++ src/main/java/com/example/RecipeGeneratorBackEnd/RecipeGeneratorBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.RecipeGeneratorBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeGeneratorBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeGeneratorBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RecipeGeneratorBackEnd/RecipeGeneratorBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RecipeGeneratorBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeGeneratorBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
